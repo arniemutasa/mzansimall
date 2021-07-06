1819dde31c8aa08a20087d1c0fb8eae42aa7284a
arniemutasa <bantuhacker@gmail.com>
2021-07-06 16:59:26 +0200

    Fixed dotenv condition bug in server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ process.on("uncaughtException", (err) => {
 });
 
 //COnfig file
-if (process.env.NODE_ENV === "PRODUCTION") {
+if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({ path: "backend/config/config.env" });
 }
 
